a4ea9287e64d6cc5ebf03ea5c56de03d520c1bcb
Biu-Parse <b.parseghian1991@gmail.com>
2025-04-06 18:31:37 -0700

    ready for review

---
+++ warm-ups.js
@@ -0,0 +1,5 @@
+const printGreeting = ( name) => `Hello there, ${name}!`
+console.log(printGreeting("Slimer"));
+
+const reverseWordOrder = (string) => string.split(" ").reverse().join(" ");
+console.log(reverseWordOrder("Ishmael me Call"));
