335ad71f53f12436069910e271aae6ce8dad452b
Biu-Parse <b.parseghian1991@gmail.com>
2025-04-06 20:21:51 -0700

    js warmup homework 4/6/25

--- scratcg.js
+++ scratcg.js
@@ -41,3 +41,166 @@ console.log("This is >>", typeof undefined) // This is random
 // Everything in javascript is true, except
 // "", 0, null, undefined, false, NaN - Not a Number
 
+///////////////////////////////////
+// * CONTROL FLOW
+///////////////////////////////////
+
+// control flow - is the order in which individual statements or instructions are executed.
+// this lets you specify when and which lines of code get executed
+
+// There are 3 forms of control flow
+// 1. Functions - save and later execute lins of code when we invoke/call said function
+// 2. Loops - repeat lines of code, for, while, recursion
+// 3. Conditionals - skip lines of code
+
+// ? Conditionals
+///////////////////////////////////
+// conditional === boolean expression, a boolean expression evaluates to either true or false
+if (conditional = true) {
+    // execution context, code to run when the conditional is true
+    console.log("Im the truth")
+}
+
+// chose and M&M
+let isMMColorGreen = true
+
+// Decide what to do based on M&M color
+if (isMMColorGreen) {
+    // true code
+    // throw the M&M away
+    console.log("Eww, I am getting rid of the gross green M&M.")
+} else {
+    // false code
+    // KEEP That one
+    console.log('Yum, YUm, YUMMMMM!')
+}
+
+
+// If there is a party let's go to it. Else if we have a coupon for glow in the dark mini golf let's do that.
+// Else if there is a sale at the mall, let's fo there. else, let's bake some cookies.
+
+const isPartyTonight = false;
+const miniGolfCoupon = true;
+const saleAtTheMall = true;
+
+if (isPartyTonight) {
+
+    console.log(`I am going to party like it's 1999`)
+} else if (miniGolfCoupon) {
+
+    console.log('Hole in one, Woo!')
+} else if (saleAtTheMall) {
+
+    console.log('When you buy more, you save more!')
+} else {
+    // base case, when all other ifs fail
+    console.log("I love cooking cookies")
+}
+
+// Explain the following code
+
+let mmColor = 'blue';
+
+// = is the assignment operator
+// == loose equality
+if ( ((mmColor == 'green')) ) {
+    console.log("Eww!, green m&m")
+} else {
+    console.log("Yum! I love M&M's")
+}
+
+// First, walkthrough it and guess what should happen
+// run the code
+// is it what you expected?
+// how can we change this code to run as expected?
+
+// === strict equality
+if ( 'green' === (mmColor = 'green') ) {
+    console.log("Eww!, green m&m")
+} else {
+    console.log("Yum! I love M&M's")
+}
+
+// Modulo operator
+// % - returns the remainder of euclidean division, it is the remainder
+
+if (4 % 2 === "0") {
+    console.log('Number is Even')
+} else {
+    console.log('Number is odd')
+}
+
+/////////////////////////////
+// ! Loops
+/////////////////////////////
+
+// Loops - allow us to do something repeatedly.
+// ++ - is the increment operator, and it does matter which side it is on. The left is the pre-increment, the right is the post-increment
+for (let step = 0; step < 5; step++) {
+    console.log(step, "<< This is our step")
+};
+
+// Do something with even numbers and add all of the odd numbers.
+
+let oddSum = 0;
+
+for (let i = 0; i < 10; i++) {
+
+    if ( "1" === i) {
+        console.log('the number is 1')
+        continue;
+    }
+
+    if ( 0 === (i % 2)) {
+        console.log('the number is even')
+        continue;
+    }
+
+    oddSum += i
+}
+
+console.log(oddSum)
+
+////////////////////////////
+// * Functions
+////////////////////////////
+
+// Functions are like spells. It can be used at any time and does magical things like add numbers, create objects and more.
+
+helloWorld() // invocation, call the function, use the function
+
+// Classic function - Hoisted, generates a prototype (constructor, arguments object)
+// parameters are in the function declaration
+function helloWorld() {
+    console.log("Hello World")
+}
+
+// Anonymous Function - NOT hoisted, generates a prototype
+const helloWorld2 = function() {
+    console.log("Hello World 2")
+    // return "Hello World"
+}
+
+helloWorld2()
+
+// Arrow Functions - NOT hoisted, no prototype
+const helloWorld3 = () => {
+    console.log("Hello World 3")
+}
+
+helloWorld3();
+
+// Immediately Invoked Function Expression - IIFE ("IFFY"), NOT hoisted, no prototype
+(function(){
+    console.log("Hello from the other side")
+})();
+
+// Parameters - are what we call them in the declaration statement
+function something(parameter1, parameter2) {
+    console.log(parameter1, "<< this is number 1")
+    console.log(parameter2, "<< this is number 2")
+}
+
+// They become arguments when invoking the function, or calling the function, or using the function
+something("I'm first", 2) // use a comma to separate params
+something({name:"kyle"}, [2,3,4,5,6])
--- warm-ups.js
+++ warm-ups.js
@@ -1,5 +1,30 @@
+//Print Greeting
 const printGreeting = ( name) => `Hello there, ${name}!`
 console.log(printGreeting("Slimer"));
 
+//Reverse Word Order
 const reverseWordOrder = (string) => string.split(" ").reverse().join(" ");
 console.log(reverseWordOrder("Ishmael me Call"));
+//Calculate
+const calculate = (num1, operation, num2) => {
+    if(operation === "add" || operation === "+"){
+        return num1+num2;
+    }
+    else if(operation === "sub" || operation === "-"){
+        return num1-num2;
+    }
+    else if(operation === "mult" || operation === "*"){
+        return num1*num2;
+    }
+    else if(operation === "div" || operation === "/"){
+        return num1/num2;
+    }
+    else if(operation === "exp" || operation === "%"){
+        return num1%num2;
+    }
+}
+console.log(calculate(4,"+", 3));
+
+//Letter Reverse
+const letterReverse =(string) => string.split("").reverse().join("");
+console.log(letterReverse("Aren't you a little short for a storm trooper"));
